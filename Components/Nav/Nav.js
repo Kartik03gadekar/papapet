419266104a6b21d0d5f9e8155d9824536ebac62e
Akshansh Sharma <aksh11ansh@gmail.com>
2025-02-18 16:09:29 +0530

    Update Nav.js

--- Components/Nav/Nav.js
+++ Components/Nav/Nav.js
@@ -4,172 +4,124 @@ import { Button } from "@mui/material";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import { Avatar, Box, IconButton, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
+import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
+
 const Nav = () => {
-  const settings = [
-    {
-      name: "Profile",
-      link: "/mediensure/profile",
-    },
-  ];
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
-  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [anchorElUser, setAnchorElUser] = useState(null);
-  const { loading, message } = useSelector((state) => state.auth);
-  useEffect(() => {}, [loading, message]);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+  useEffect(() => {
+    dispatch(checkUser());
+  }, [dispatch]);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
   };
+
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  useEffect(() => {
-    dispatch(checkUser());
-  }, []);
 
   return (
     <>
-      <div className="h-[10vh] z-50 p-5  bg-white text-sm font-light fixed  w-full  shadow-md items-center justify-between   flex gap-5">
-        <div className="flex md:hidden  gap-5">
-          <img className="h-7 mt-4" src="/menu.png" alt="" />
-          <Link href={"/"} className="flex items-center justify-center gap-2">
-            <img className="object-contain h-10 " src="/logo.png" alt="" />
+      {/* Navbar Container */}
+      <div className="h-[10vh] z-50 p-5 bg-white text-sm font-light fixed w-full shadow-md flex items-center justify-between">
+        {/* Mobile Menu Button */}
+        <div className="flex md:hidden items-center gap-5">
+          <IconButton onClick={toggleMobileMenu}>
+            {mobileMenuOpen ? <CloseIcon fontSize="large" /> : <MenuIcon fontSize="large" />}
+          </IconButton>
+          <Link href={"/"} className="flex items-center gap-2">
+            <img className="h-10 object-contain" src="/logo.png" alt="PaPaPet Logo" />
             <h1 className="font-[monument]">PaPaPet</h1>
           </Link>
         </div>
-        <div className=" flex pl-24 items-center gap-2   max-md:hidden  ">
-          <Link href={"/"} className="flex items-center justify-center gap-2">
-            <img className="object-contain h-7 " src="/logo.png" alt="" />
+
+        {/* Desktop Navbar */}
+        <div className="hidden md:flex pl-24 items-center gap-5">
+          <Link href={"/"} className="flex items-center gap-2">
+            <img className="h-7 object-contain" src="/logo.png" alt="PaPaPet Logo" />
             <h1 className="font-[poppins] font-semibold">PaPaPet</h1>
           </Link>
-          <Link className="" href={"/mediensure/consultdr"}>
-            {" "}
-            Book Consultation
-          </Link>
-          <Link className="" href={"/mediensure/Newdemo4"}>
-            {" "}
-            Our Network
-          </Link>
-          <Link className="" href={"/mediensure/ivfnetwork"}>
-            {" "}
-            Ivf Network
-          </Link>
+          <Link href={"/mediensure/consultdr"}>Book Consultation</Link>
+          <Link href={"/mediensure/Newdemo4"}>Our Network</Link>
+          <Link href={"/mediensure/ivfnetwork"}>Ivf Network</Link>
         </div>
-        <div className="flex  items-center justify-center pt-2 pr-14 gap-5 max-md:hidden">
-          <Link href={"mediensure/demo2"} className="flex items-center ">
-            For Corporate
-            <img className="h-5" src="/down.png" alt="" />
+
+        {/* User & Authentication Section */}
+        <div className="hidden md:flex items-center gap-5 pr-14">
+          <Link href={"/mediensure/demo2"} className="flex items-center">
+            For Corporate <img className="h-5" src="/down.png" alt="" />
           </Link>
-          <Link href={"/mediensure/demo"} className=" flex items-center ">
-            For Providers
-            <img className="h-5" src="/down.png" alt="" />
+          <Link href={"/mediensure/demo"} className="flex items-center">
+            For Providers <img className="h-5" src="/down.png" alt="" />
           </Link>
           {user ? (
-            <>
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar
-                      alt="Remy Sharp"
-                      src="/static/images/avatar/2.jpg"
-                    />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {settings.map((setting) => (
-                    <>
-                      <Link href={`${setting.link}`}>
-                        <MenuItem
-                          key={setting.name}
-                          onClick={handleCloseUserMenu}
-                        >
-                          <Typography textAlign="center">
-                            {setting.name}
-                          </Typography>
-                        </MenuItem>
-                      </Link>
-                      <MenuItem
-                        key="Logout"
-                        onClick={() => dispatch(logoutUser())}
-                      >
-                        <Typography textAlign="center">Logout</Typography>
-                      </MenuItem>
-                    </>
-                  ))}
-                </Menu>
-              </Box>
-              <h1 className="font-semibold text-black">Hi, {user?.name}</h1>
-            </>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                anchorEl={anchorElUser}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+              >
+                <Link href="/mediensure/profile">
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Profile</Typography>
+                  </MenuItem>
+                </Link>
+                <MenuItem onClick={() => dispatch(logoutUser())}>
+                  <Typography textAlign="center">Logout</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
           ) : (
             <Link href={"/mediensure/auth"}>
-              {" "}
-              <Button variant="outlined" className="h-fit w-fit  ">
-                Login/Register{" "}
-              </Button>
+              <Button variant="outlined">Login/Register</Button>
             </Link>
           )}
         </div>
       </div>
 
-      {/* @media (min-width: 500px) { 
-         <div className='h-16 z-50 p-5  bg-white text-sm font-light fixed  w-full  shadow-md items-center justify-between   flex gap-5'>
-         <div  className=' flex pl-24 items-center gap-4     '>
-             <img className='object-contain h-10 ' src="/logo.png" alt="" />
-     <Link className='' href={""}> Find Doctors</Link>
-     <Link className='' href={""}> Find Doctors</Link>
-     <Link className='' href={""}> Find Doctors</Link>
-     <Link className='' href={""}> Find Doctors</Link>
-     <Link className='' href={""}> Find Doctors</Link>
-             </div>
-             <div className='flex  items-center justify-center pt-2 pr-14 gap-5'>
-                 <Link href={""} className='flex items-center '>For Corporate 
-                 <img className='h-5' src="/down.png" alt="" />
-                 </Link>
-                 <Link href={""} className=' flex items-center '>For Providers
-                 <img className='h-5' src="/down.png" alt="" />
-                 </Link>
-     <Link href={"/mediensure/auth"}>        <Button variant='outlined' className='h-fit w-fit '>Login/Register  </Button></Link>
-     
-             </div>
-         </div>
-    
-    } */}
+      {/* Mobile Menu Drawer */}
+      <div className={`fixed inset-0 bg-white z-50 transition-transform duration-300 ${mobileMenuOpen ? "translate-x-0" : "translate-x-full"} md:hidden`}>
+        <div className="absolute top-5 right-5">
+          <IconButton onClick={toggleMobileMenu}>
+            <CloseIcon fontSize="large" />
+          </IconButton>
+        </div>
+        <nav className="flex flex-col items-center justify-center gap-6 text-xl font-semibold mt-16">
+          <Link href="/" onClick={toggleMobileMenu}>Home</Link>
+          <Link href="/mediensure/consultdr" onClick={toggleMobileMenu}>Book Consultation</Link>
+          <Link href="/mediensure/Newdemo4" onClick={toggleMobileMenu}>Our Network</Link>
+          <Link href="/mediensure/ivfnetwork" onClick={toggleMobileMenu}>Ivf Network</Link>
+          <Link href="/mediensure/demo2" onClick={toggleMobileMenu}>For Corporate</Link>
+          <Link href="/mediensure/demo" onClick={toggleMobileMenu}>For Providers</Link>
+          {user ? (
+            <>
+              <Link href="/mediensure/profile" onClick={toggleMobileMenu}>Profile</Link>
+              <button onClick={() => { dispatch(logoutUser()); toggleMobileMenu(); }} className="text-red-500">
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link href="/mediensure/auth" onClick={toggleMobileMenu}>Sign In</Link>
+          )}
+        </nav>
+      </div>
     </>
   );
 };
