b97620438d72a7c404d8b8db8e8fd4a40d79c9ae
first <garvitjainraju@gmail.com>
2025-02-19 02:00:09 +0530

    update

--- app/page.js
+++ app/page.js
@@ -4,22 +4,40 @@ import dynamic from "next/dynamic";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getHomePage } from "@/store/Action/others";
-import socket from "@/utils/socket";
+import { io } from "socket.io-client";
+// import socket from "@/utils/socket";
+let socket;
+const getSocket = () => {
+  if (!socket) {
+    // socket = io("http://localhost:5001/", { path: "/socket.io" });
+    socket = io("https://api.mycozee.in/", {
+      path: "/socket.io",
+    });
+  }
+  return socket;
+};
 
 // Dynamically import heavy components for performance optimization
-const NavPapaPet = dynamic(() => import("@/Components/Nav/NavPapaPet"), { ssr: false });
-const HomePage = dynamic(() => import("@/Components/HomePage/HomePage"), { ssr: false });
+const NavPapaPet = dynamic(() => import("@/Components/Nav/NavPapaPet"), {
+  ssr: false,
+});
+const HomePage = dynamic(() => import("@/Components/HomePage/HomePage"), {
+  ssr: false,
+});
 const Page2 = dynamic(() => import("@/Components/Page2/Page2"), { ssr: false });
 const Page3 = dynamic(() => import("@/Components/Page3/Page3"), { ssr: false });
 const Page4 = dynamic(() => import("@/Components/Page4/Page4"), { ssr: false });
 const Page5 = dynamic(() => import("@/Components/Page5/Page5"), { ssr: false });
-const Footer = dynamic(() => import("@/Components/Footer/Footer"), { ssr: false });
+const Footer = dynamic(() => import("@/Components/Footer/Footer"), {
+  ssr: false,
+});
 
 const Page = () => {
   const dispatch = useDispatch();
   const { homepage, imgLink } = useSelector((state) => state.others);
 
   useEffect(() => {
+    getSocket()
     dispatch(getHomePage());
   }, [dispatch]);
 
@@ -50,4 +68,3 @@ const Page = () => {
 };
 
 export default Page;
-
