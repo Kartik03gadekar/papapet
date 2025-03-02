42d686324409633ade8e4b86c2d728018e85bf50
first <garvitjainraju@gmail.com>
2025-03-02 21:18:32 +0530

    update

--- app/mediensure/auth/page.js
+++ app/mediensure/auth/page.js
@@ -1,15 +1,15 @@
-import Auth from '@/Components/Login/Auth'
-import Login from '@/Components/Login/Login'
-import Nav from '@/Components/Nav/Nav'
-import React from 'react'
+import Auth from "@/Components/Login/Auth";
+import Login from "@/Components/Login/Login";
+import Nav from "@/Components/Nav/Nav";
+import React from "react";
 
 const page = () => {
   return (
     <div>
       {/* <Nav/> */}
-<Auth/>
+      <Auth />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
