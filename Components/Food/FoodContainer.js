feaf2927ae4e1ed613e929c10ef0828f753b403b
first <garvitjainraju@gmail.com>
2025-03-09 14:58:24 +0530

    window

--- Components/Food/FoodContainer.js
+++ Components/Food/FoodContainer.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../ProductCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -10,7 +10,7 @@ import { Pagination, Navigation } from "swiper/modules";
 import { useDispatch, useSelector } from "react-redux";
 import { getFood } from "@/store/Action/others";
 import Loading from "../loading";
-import style from "./FoodContainer.module.css"
+import style from "./FoodContainer.module.css";
 
 const FoodContainer = () => {
   const data = [
@@ -70,8 +70,20 @@ const FoodContainer = () => {
   ];
   const { load, food, imgLink } = useSelector((state) => state.others);
   const dispatch = useDispatch();
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
   useEffect(() => {
     dispatch(getFood());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 768);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
   const d = [
     {
@@ -94,95 +106,93 @@ const FoodContainer = () => {
   return (
     <div className="w-full min-h-screen text-black  py-6 mt-[5vh] ">
       {/* Browse By Brands */}
- <div className="flex flex-col gap-[4vw] items-center justify-center mt-2  ">
- <div className=" flex  items-center text-center justify-center">
-    <h1 className="text-4xl font-semibold relative z-10 bg-white text-center max-md:text-3xl max-md:pb-6">
-      Browse By Brands
-    </h1>
-  </div>
-<div className="w-full  h-52 max-md:h-40 flex  items-center justify-center gap-8 py-2 ">
-  
-  <Swiper
-    slidesPerView={3}
-    spaceBetween={20}
-    breakpoints={{
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 4 },
-    }}
-    navigation={window?.innerWidth > 768}
-    pagination={{ clickable: true }}
-    modules={[Pagination, Navigation]}
-    className={`w-full  ${style.swiper}`}
-  >
-    {brands.map((brand, index) => (
-      <SwiperSlide
-        key={index}
-       
-      >
-
-       <div className="w-[20vw]  max-md:w-[30vw] flex flex-col items-center justify-center ">
-       <div className="h-[20vh] w-[20vh] max-md:h-[10vh] max-md:w-[10vh] rounded-full border-2 overflow-hidden
-        flex  flex-col items-center justify-center  ">
-          <img
-            src={brand.img}
-            className="w-full h-full object-cover  "
-            alt={brand.name}
-          />
-           
+      <div className="flex flex-col gap-[4vw] items-center justify-center mt-2  ">
+        <div className=" flex  items-center text-center justify-center">
+          <h1 className="text-4xl font-semibold relative z-10 bg-white text-center max-md:text-3xl max-md:pb-6">
+            Browse By Brands
+          </h1>
         </div>
-        <h1 className="font-semibold max-md:text-sm mt-2">{brand.name}</h1>
-       </div>
-       
-      </SwiperSlide>
-    ))}
-  </Swiper>
-</div>
- </div>
-
-{/* Browse By Category */}
-<div className="flex flex-col gap-[4vw] items-center justify-center mt-[10vw] max-md:mt-[14vw] ">
- <div className=" flex  items-center text-center justify-center">
-    <h1 className="text-4xl font-semibold relative z-10 bg-white text-center max-md:text-3xl max-md:pb-6">
-      Browse By Category
-    </h1>
-  </div>
-<div className="w-full  h-52 max-md:h-40 flex  items-center justify-center gap-8 py-2 ">
-  
-<Swiper
-    slidesPerView={3}
-    spaceBetween={20}
-    breakpoints={{
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 4 },
-    }}
-    navigation={window?.innerWidth > 768}
-    pagination={{ clickable: true }}
-    modules={[Pagination, Navigation]}
-    className={`w-full  ${style.swiper}`}
-  >
-    {d.map((category, index) => (
-      <SwiperSlide
-        key={index}
-        className="flex flex-col items-center justify-center text-center"
-      >
-      <div className="w-[20vw]  max-md:w-[30vw] flex flex-col items-center justify-center ">
-       <div className="h-[20vh] w-[20vh] max-md:h-[10vh] max-md:w-[10vh] rounded border-2 overflow-hidden
-        flex  flex-col items-center justify-center  ">
-          <img
-            src={`/${category.img}`}
-            className="h-full w-full object-contain"
-            alt={category.name}
-          />
+        <div className="w-full  h-52 max-md:h-40 flex  items-center justify-center gap-8 py-2 ">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={20}
+            breakpoints={{
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 4 },
+            }}
+            navigation={isLargeScreen}
+            pagination={{ clickable: true }}
+            modules={[Pagination, Navigation]}
+            className={`w-full  ${style.swiper}`}
+          >
+            {brands.map((brand, index) => (
+              <SwiperSlide key={index}>
+                <div className="w-[20vw]  max-md:w-[30vw] flex flex-col items-center justify-center ">
+                  <div
+                    className="h-[20vh] w-[20vh] max-md:h-[10vh] max-md:w-[10vh] rounded-full border-2 overflow-hidden
+        flex  flex-col items-center justify-center  "
+                  >
+                    <img
+                      src={brand.img}
+                      className="w-full h-full object-cover  "
+                      alt={brand.name}
+                    />
+                  </div>
+                  <h1 className="font-semibold max-md:text-sm mt-2">
+                    {brand.name}
+                  </h1>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-        <h1 className="font-semibold max-md:text-sm mt-2">{category.name}</h1>
-   </div>
-      </SwiperSlide>
-    ))}
-  </Swiper>
+      </div>
 
-  </div>
-  </div>
-
+      {/* Browse By Category */}
+      <div className="flex flex-col gap-[4vw] items-center justify-center mt-[10vw] max-md:mt-[14vw] ">
+        <div className=" flex  items-center text-center justify-center">
+          <h1 className="text-4xl font-semibold relative z-10 bg-white text-center max-md:text-3xl max-md:pb-6">
+            Browse By Category
+          </h1>
+        </div>
+        <div className="w-full  h-52 max-md:h-40 flex  items-center justify-center gap-8 py-2 ">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={20}
+            breakpoints={{
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 4 },
+            }}
+            navigation={isLargeScreen}
+            pagination={{ clickable: true }}
+            modules={[Pagination, Navigation]}
+            className={`w-full  ${style.swiper}`}
+          >
+            {d.map((category, index) => (
+              <SwiperSlide
+                key={index}
+                className="flex flex-col items-center justify-center text-center"
+              >
+                <div className="w-[20vw]  max-md:w-[30vw] flex flex-col items-center justify-center ">
+                  <div
+                    className="h-[20vh] w-[20vh] max-md:h-[10vh] max-md:w-[10vh] rounded border-2 overflow-hidden
+        flex  flex-col items-center justify-center  "
+                  >
+                    <img
+                      src={`/${category.img}`}
+                      className="h-full w-full object-contain"
+                      alt={category.name}
+                    />
+                  </div>
+                  <h1 className="font-semibold max-md:text-sm mt-2">
+                    {category.name}
+                  </h1>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
 
       {/* <div className="hidden w-full px-[10vh] h-[60vh]  items-center justify-center  mt-[2vw] max-md:mt-[4vw] max-md:flex-col max-md:px-[4v h]"  >
         <div className="flex flex-col gap-4 max-md:w-full">
@@ -199,7 +209,9 @@ const FoodContainer = () => {
         <img src="/explore.png" alt="" />
       </div> */}
       <div className="flex flex-col items-center justify-center py-10 gap-10">
-        <h1 className="text-4xl font-semibold max-md:text-3xl ">Explore our all Products</h1>
+        <h1 className="text-4xl font-semibold max-md:text-3xl ">
+          Explore our all Products
+        </h1>
         <div className="w-full grid grid-cols-4 px-10 max-md:grid-cols-2 max-md:w-full max-md:rounded-xl max-md:gap-4 max-md:px-4">
           {food?.map((i, index) => (
             <ProductCard key={index} i={i} imgLink={imgLink} />
