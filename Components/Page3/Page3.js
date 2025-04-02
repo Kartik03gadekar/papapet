71cd793c957486c2fee4747e7d63bbadf73e9388
lokeshdhote <lokeshdhote2004@gmail.com>
2025-04-02 16:41:17 +0530

    typesof category

--- Components/Page3/Page3.js
+++ Components/Page3/Page3.js
@@ -32,7 +32,8 @@ const Page3 = () => {
       {/* Category Section */}
       <div className="flex items-center justify-between max-md:flex  max-md:items-center   max-md:justify-around  max-md:w-screen w-[80%] max-md:px-[2vw]  ">
         {data?.map((i, index) => (
-          <Link key={index} href={`${i.link || "#"}`}>
+           
+          <Link key={index} href={`/papapet/product/type/${i.name}`}>
             <div className="flex flex-col items-center gap-6 cursor-pointer">
               {/* Image Container */}
               <div className="h-[13vw] w-[13vw] max-md:h-[25vw] max-md:w-[25vw] rounded-full overflow-hidden border-2  max-md:border-none flex items-center justify-center hover:shadow-lg transition-all duration-300">
