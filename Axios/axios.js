44c3db73dc821710c5394a4f19b1542bc0040ed9
lokeshdhote <lokeshdhote2004@gmail.com>
2025-07-08 13:59:52 +0530

    8-7-9

--- Axios/axios.js
+++ Axios/axios.js
@@ -1,11 +1,41 @@
+// import axios from "axios";
+
+// const instance = axios.create({
+//     baseURL:"https://papapetbackend-1.onrender.com/api/v1/",
+//     // baseURL:"http://localhost:8080/api/v1/",
+
+//     withCredentials:true
+// })
+
+// export default instance;
+
+
 import axios from "axios";
 
+// Create an axios instance
 const instance = axios.create({
-    baseURL:"https://papapetbackend-1.onrender.com/api/v1/",
-    // baseURL:"http://localhost:8080/api/v1/",
+  // Base URL for backend API (adjust for prod/dev)
+  baseURL: "https://papapetbackend-1.onrender.com/api/v1/",
+  // If using localhost for dev:
+  // baseURL: "http://localhost:8080/api/v1/",
 
-    withCredentials:true
-})
+  withCredentials: true, // Send cookies with every request
+});
+
+// Interceptor to add Authorization token (optional)
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("papapetToken");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
-
