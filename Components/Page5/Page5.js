c3359e34fd1e44d18277fa43b4faea513052c4b9
AsYnc <talhafaizan123456@gmail.com>
2025-08-09 10:16:04 +0530

    Update Page5.js

--- Components/Page5/Page5.js
+++ Components/Page5/Page5.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page5 = () => {
   return (
-    <div className=" max-md:hidden  w-full h-fit p-20 flex flex-col items-center justify-center max-md:px-0 bg-red-400">
+    <div className=" max-md:hidden  w-full h-fit p-20 flex flex-col items-center justify-center max-md:px-0">
     <img src="/aboutUs.png" className="w-[65vw] object-contain max-md:object-cover max-md:h-[18vh] max-md:w-[120%] " alt="" />
      
     </div>
