85ccc0e7e8423448a5e00ab5bad54b70184ca45f
lokeshdhote <lokeshdhote2004@gmail.com>
2025-08-16 21:05:32 +0530

    16.newlybed

--- Axios/axios.js
+++ Axios/axios.js
@@ -15,8 +15,8 @@ import axios from "axios";
 // Create an axios instance
 const instance = axios.create({
 
-  baseURL: "https://papapetbackend-1.onrender.com/api/v1/",
-
+  // baseURL: "https://papapetbackend-1.onrender.com/api/v1/",
+baseURL: "https://papapetbackend-qxyx.onrender.com/api/v1/",
   // baseURL: "http://localhost:8080/api/v1/",
 
   withCredentials: true, 
