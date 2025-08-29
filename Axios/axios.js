f83cdbe488187cfb9cc8f41495ea030839d42168
AsYnc <talhafaizan123456@gmail.com>
2025-08-30 03:10:13 +0530

    iphone test

--- Axios/axios.js
+++ Axios/axios.js
@@ -14,7 +14,8 @@ import axios from "axios";
 
 // Create an axios instance
 const instance = axios.create({
-  baseURL: "https://papapetbackend-oaiw.onrender.com/api/v1/",
+  // baseURL: "https://papapetbackend-oaiw.onrender.com/api/v1/",
+  baseURL: "/api/v1",
   
   // baseURL: "http://localhost:8080/api/v1/",
 
