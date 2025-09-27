8dfe3e8b8600a9525ece6cda04c3be4e51cc592e
AsYnc <talhafaizan123456@gmail.com>
2025-09-28 00:13:30 +0530

    Update axios.js

--- Axios/axios.js
+++ Axios/axios.js
@@ -12,8 +12,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://papapetbackend-oaiw.onrender.com/api/v1/",
-  // baseURL: "/api/v1/",
+  // baseURL: "https://papapetbackend-oaiw.onrender.com/api/v1/",
+  baseURL: "/api/v1/",
   // baseURL: "http://localhost:8080/api/v1/",
 
   withCredentials: true,
