445a6b4a4db4401d681835904874266ab7d0a933
AsYnc <talhafaizan123456@gmail.com>
2025-10-04 21:24:20 +0530

    pixel done

--- app/layout.js
+++ app/layout.js
@@ -9,26 +9,16 @@ import NextTopLoader from "nextjs-toploader";
 import TransitionWrapper from "@/Components/TransitionWrapper";
 import ReduxProvider from "@/store/ReduxProvider";
 import Script from "next/script";
+import FacebookPixel from "@/Components/FacebookPixel";
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_PIXEL_ID');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+
+
       </head>
       <body suppressHydrationWarning={true}>
         <ReduxProvider>
@@ -47,6 +37,7 @@ export default function RootLayout({ children }) {
             theme="light"
             limit={1} // only one popup at a time
           />
+          <FacebookPixel />
         </ReduxProvider>
       </body>
     </html>
